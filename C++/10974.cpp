5be9df8b817d8324d7650bfcd775b1f6c5b0599f
thddk0317 <thddk0317@nate.com>
2019-04-09 16:16:13 +0900

    모든 순열 - dfs 이용 (시간 초과)

---
+++ C++/10974.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int numbers[10];
+int selected[10];
+
+void answer(int num, int idx) {
+	numbers[idx] = num;
+	
+	if (idx == N) {
+		for (int i = 1; i <= N; i++) {
+			cout << numbers[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (selected[i]) 
+			continue;
+		
+		selected[i] = 1;
+		answer(i, idx + 1);
+		selected[i] = 0;
+	}
+}
+
+int main(int argc, char** argv) {
+	cin >> N;
+	for (int num_cnt = 1; num_cnt <= N; num_cnt++) {
+		selected[num_cnt] = 1;
+		answer(num_cnt, 1);
+		selected[num_cnt] = 0;
+	}
+	return 0;
+}
