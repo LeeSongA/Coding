fbfed2a9d788d2cf711b40755f8263e84e26f811
thddk0317 <thddk0317@nate.com>
2019-04-17 01:33:03 +0900

    A + B - 7

---
+++ C++/11021.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int testcase;
+	cin >> testcase;
+	
+	for (int i = 1; i <= testcase; i++) {
+		int num_one, num_two;
+		cin >> num_one >> num_two;
+		cout << "Case #" << i << ": " << num_one + num_two << endl;
+	}
+	return 0;
+}
