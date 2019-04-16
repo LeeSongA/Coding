95e5dc44d0861d0485092bfd0c6582ca3e917729
thddk0317 <thddk0317@nate.com>
2019-04-16 17:53:02 +0900

    별 찍기 - 17

---
+++ C++/10992.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int n;
+	int space = 1;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		if (i == 1)
+			cout << setw(n) << "*" << endl;
+		else if (i == n)
+			for (int j = 1; j < i * 2; j++) {
+				cout << "*";
+			}
+		else {
+			cout << setw(n - i + 1) << "*";
+			for (int j = 1; j < i; j++) {
+				space = i + j;
+			}
+			cout << setw(space-1) << "*" << endl;
+		}
+	}
+	return 0;
+}
